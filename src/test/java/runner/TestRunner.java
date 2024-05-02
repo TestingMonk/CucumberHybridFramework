3ba7f68f2c4da6a053007f983812153402b58392
Sanjeev <mr.sanjeevyadav1@gmail.com>
2024-05-03 00:10:04 +0530

    runner updated with extent plugin

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,10 +6,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resource/features/Search.feature",
+@CucumberOptions(features="src/test/resources/features/Search.feature",
                  glue= {"stepdefinations","hooks"},
                  publish=true,  // gives cloud link of reports
-                 plugin ={"pretty","html:target/CucumberReports/CucumberReport.html"}  // result in nice detailed format 
+                 plugin ={"pretty","html:target/CucumberReports/CucumberReport.html", // result in nice detailed format
+                		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", //extent report
+         				}  
                  
 		)
                  
